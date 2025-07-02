5bedc07264b6d02a0c59a3bfb39806543b680d8b
Arash <arashn2y@gmail.com>
2025-07-02 13:23:55 +0200

    lezione 02/07

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,17 @@
+import type { ReactNode } from "react";
+
 interface ButtonProps {
   title: string;
   onClick: () => void;
-  children: string;
+  children: ReactNode;
 }
 
 function Button(props: ButtonProps) {
   // const { title, onClick } = props;
   return (
-    <button className="w-24 h-10 rounded-lg bg-black hover:bg-black-700 text-white cursor-pointer" onClick={props.onClick}>
+    <button
+      className="flex items-center gap-1 px-2 h-10 rounded-lg hover:bg-black hover:bg-black-700 text-white cursor-pointer"
+      onClick={props.onClick}>
       {props.children}
     </button>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,36 @@
 import { useState } from "react";
+import { RiLoginCircleLine as LoginIcon } from "react-icons/ri";
 import Button from "./Button";
-import Input from "./Input";
+// import Input from "./Input";
 
 function Header() {
-  const [count, setCount] = useState(0);
-  const [value, setValue] = useState("");
+  const [count, setCount] = useState<number>(0);
+  // const [value, setValue] = useState("");
 
   const clickHandler = () => {
     setCount(count + 1);
   };
 
-  const changeHandler = (value: string) => {
-    setValue(value);
-  };
+  // const changeHandler = (value: string) => {
+  //   setValue(value);
+  // };
 
   return (
-    <nav className="flex flex-col justify-between items-center mt-4">
+    <header className="bg-gray-200 flex justify-between items-center p-4">
       <h1>My Website</h1>
-      <Input
+      {/* <Input
         value={value}
         onChange={(inputValue: string) => {
           changeHandler(inputValue);
         }}
-      />
-      <Button title="Click me" onClick={clickHandler} children="pippo" />
-      {/* <Button title="Click me" onClick={clickHandler}>
-        pippo
-      </Button> */}
-      <h1 className="">{count} count</h1>
-    </nav>
+      /> */}
+      {/* <Button title="Click me" onClick={clickHandler} children="Login" /> */}
+      <Button title="Click me" onClick={clickHandler}>
+        <LoginIcon />
+        {/* Login */}
+      </Button>
+      {/* <h1 className="">{count} count</h1> */}
+    </header>
   );
 }
 
